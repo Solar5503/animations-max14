c034a8a55427b3affc67838bf7a45768e87baa83
Solar5503 <spidervvv@mail.ru>
2022-11-17 17:03:00 +0300

    add transition component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Transition } from 'react-transition-group';
 
 import './App.css';
 import Backdrop from './components/Backdrop/Backdrop';
@@ -8,6 +9,7 @@ import Modal from './components/Modal/Modal';
 class App extends Component {
   state = {
     modalIsOpen: false,
+    showBlock: false,
   };
 
   showModal = () => this.setState({ modalIsOpen: true });
@@ -17,6 +19,36 @@ class App extends Component {
     return (
       <div className="App">
         <h1>React Animations</h1>
+        <button
+          className="Button"
+          style={{ marginBottom: 20 }}
+          onClick={() =>
+            this.setState((prevState) => ({ showBlock: !prevState.showBlock }))
+          }
+        >
+          Toggle
+        </button>
+        <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: 'red',
+                width: 100,
+                height: 100,
+                margin: 'auto',
+                marginBottom: 20,
+                transition: 'opacity 1s ease-out',
+                opacity: state === 'exiting' ? 0 : 1,
+              }}
+            ></div>
+          )}
+        </Transition>
         <Modal closed={this.closeModal} show={this.state.modalIsOpen} />
         <Backdrop show={this.state.modalIsOpen} />
         <button className="Button" onClick={this.showModal}>
